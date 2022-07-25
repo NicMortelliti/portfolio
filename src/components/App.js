06bc3ce75e98cc1451b80c39df02f30d842dbd2e
NicMortelliti <npmortelliti@gmail.com>
2022-07-25 15:25:13 -0700

    Add HeroAbout component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../style/App.css";
 
 // Import components
+import HeroAbout from "./HeroAbout";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroAbout />
     </div>
   );
 }
---
+++ src/components/HeroAbout.js
@@ -0,0 +1,14 @@
+// This component displays my name and
+// and short 2-4 word blurbs about me
+// in large font. Cycling through
+// the descriptors.
+
+import React from 'react'
+
+function HeroAbout() {
+  return (
+    <div>HeroAbout</div>
+  )
+}
+
+export default HeroAbout
