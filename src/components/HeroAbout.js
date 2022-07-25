8941c822884082a14943f7fd92d19eb76e94a2a9
NicMortelliti <npmortelliti@gmail.com>
2022-07-25 16:27:07 -0700

    Cycle through blurbs

--- src/components/HeroAbout.js
+++ src/components/HeroAbout.js
@@ -2,7 +2,7 @@
 // and short 2-4 word blurbs about me
 // in large font. Cycling through
 // the descriptors.
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const name = { first: "Nic", last: "Mortelliti" };
 
@@ -15,13 +15,29 @@ const blurbs = [
 ];
 
 function HeroAbout() {
+  const [currentBlurb, setCurrentBlurb] = useState(blurbs[0]);
+
+  // Cycle through blurbs
+  useEffect(() => {
+    // Initialize iterator to 0
+    let i = 0;
+
+    // Every n-seconds, change the current blurb state
+    setInterval(() => {
+      setCurrentBlurb(blurbs[i]);
+
+      // Reset iterator to 0 if end of array is reached
+      i < blurbs.length - 1 ? (i += 1) : (i = 0);
+    }, 2000);
+  }, []);
+
   return (
     <div>
       <h1>{`My name is ${name.first} ${name.last}`}</h1>
       <h3>I am</h3>
-      <h2>{blurbs[0]}</h2>
+      <h2>{currentBlurb}</h2>
     </div>
   );
 }
 
-export default HeroAbout
+export default HeroAbout;
