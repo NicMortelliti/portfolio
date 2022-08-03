347db4bdcd764ea270d8f7a82b4ad12f3f21aac6
Nic Mortelliti <npmortelliti@gmail.com>
2022-08-03 08:23:30 -0700

    Add basic theme switching

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Themes";
 import "../style/App.css";
 
 // Import components
@@ -7,13 +11,23 @@ import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
   return (
-    <div className="App">
-      <Navbar />
-      <HeroAbout />
-      <CallToAction />
-      <Portfolio />
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <button onClick={themeToggler}>Switch Theme</button>
+          <Navbar />
+          <HeroAbout />
+          <CallToAction />
+          <Portfolio />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
