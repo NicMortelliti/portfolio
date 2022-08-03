13a32e6cbf6dfcd7d106fee61c45ee9c368ca648
Nic Mortelliti <npmortelliti@gmail.com>
2022-08-03 07:27:25 -0700

    Start configuring Themes component

---
+++ src/components/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+  body: "#1b2232",
+  text: "#d7ccb1",
+  toggleBorder: "#1b2232",
+  background: "#c49bbb"
+};
+
+export const darkTheme = {
+  body: '#363537',
+  text: '#FAFAFA',
+  
+}
