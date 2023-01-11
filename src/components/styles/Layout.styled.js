a2f3478591144514d257771f68184aff25d09b55
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-11 14:31:43 -0800

    moved theme to themeprovider

--- src/components/styles/Layout.styled.js
+++
@@ -1,9 +0,0 @@
-const sixtyColor = "rgba(27, 34, 50, 1)";
-const thirtyColor = "rgba(196, 155, 187, 1)";
-const tenColor = "rgba(215, 204, 177, 1)";
-
-export const theme = {
-  bgColor: sixtyColor,
-  color: tenColor,
-  accent: thirtyColor,
-};
