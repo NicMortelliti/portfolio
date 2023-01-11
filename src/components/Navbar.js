f3dce5651967405d2961ef921adff041fd1af1d5
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-11 12:50:10 -0800

    Remove resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,12 @@ import React from "react";
 import Button from "./Button";
 
 // External Links
-const resumeURL =
-  "https://drive.google.com/file/d/14YPdAxf0GCHst7gSb9p4TZAn32ud2GfI/view?usp=sharing";
 const linkedInURL = "https://www.linkedin.com/in/nicolas-mortelliti/";
 const githubURL = "https://github.com/NicMortelliti";
 
 function Navbar() {
   return (
     <div>
-      <Button url={resumeURL} value="Resume" ui="social" />
       <Button url={linkedInURL} value="LinkedIn" ui="social" />
       <Button url={githubURL} value="Github" ui="social" />
     </div>
