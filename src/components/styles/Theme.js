e20fec7c28fdfed5fe33f7cd68551e7ce42dfa73
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-13 18:59:36 -0800

    Add color scheme

--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -2,6 +2,18 @@ const sixtyColor = "rgba(27, 34, 50, 1)";
 const thirtyColor = "rgba(196, 155, 187, 1)";
 const tenColor = "rgba(215, 204, 177, 1)";
 
+// Black Panter colors
+const purpleLight = "#9666C1";
+const purpleDark = "#750E81";
+const yellowLight = "#CDA95C";
+const yellowDark = "#6F4002";
+const redLight = "#8A0008";
+const redDark = "#460001";
+const greenLight = "#11E5A4";
+const greenDark = "#073530";
+const blueLight = "#32AAA8";
+const blueDark = "#07525B";
+
 export const theme = {
   bgColor: sixtyColor,
   color: tenColor,
