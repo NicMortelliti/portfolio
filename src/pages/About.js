5a9f129c5e6875caec4c3692456039ba9b6744d9
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-16 16:09:01 -0800

    add basic about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h1>Hi, I'm Nic</h1>
+      <h2>
+        I'm a newly minted software developer with a history as an avionics
+        systems engineer.
+      </h2>
+      <h2>
+        I'm also a father of 2, a husband, a hydroponic gardener, photographer
+        and pilot.
+      </h2>
+      <h2>
+        Take a look at my portfolio, if you like what you see and want to work
+        together, let's get in touch.
+      </h2>
+    </>
+  );
 };
 
 export default About;
