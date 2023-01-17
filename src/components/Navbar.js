32068b3894ba06d011d79034a25b3da45684b85b
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-16 18:14:30 -0800

    Add blog link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,19 @@ import { NavLink } from "react-router-dom";
 import { NavBar as Nav } from "./styles/NavBar.styled";
 
 // External Links
-const linkedInURL = "https://www.linkedin.com/in/nicolas-mortelliti/";
-const githubURL = "https://github.com/NicMortelliti";
+// Create an array of links with a "to" that points to the external url,
+// as well as the "text" to display.
+// Then map through the links, creating a link for each one.
+const externalLinks = [
+  { to: "https://www.linkedin.com/in/nicolas-mortelliti/", text: "LinkedIn" },
+  { to: "https://github.com/NicMortelliti", text: "Github" },
+  { to: "https://dev.to/nicm", text: "Blog" },
+];
 
+const RenderExternalLinks = () =>
+  externalLinks.map(({ to, text }) => <a href={to}>{text}</a>);
+
+// Internal Links
 // Create an array of links with a "to" that points to the react-router path,
 // as well as the "text" to display as the NavLink.
 // Then map through the links, creating a navlink for each one.
@@ -31,8 +41,7 @@ function Navbar() {
   return (
     <Nav>
       <RenderLinks />
-      <a href={linkedInURL}>LinkedIn</a>
-      <a href={githubURL}>Github</a>
+      <RenderExternalLinks />
     </Nav>
   );
 }
