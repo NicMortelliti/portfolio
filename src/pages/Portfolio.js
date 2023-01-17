20c3d1c600c7d5f067b839ead04f40f085b9abc3
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-16 16:32:48 -0800

    add basic portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const links = [
+    {
+      to: "https://moonshot.nicweb.dev",
+      text: "Moonshot",
+      description: "A fictional space flight booking site.",
+    },
+    {
+      to: "https://scheduler.nicweb.dev",
+      text: "Appointment Scheduler",
+      description: "An application for booking appointments.",
+    },
+    {
+      to: "https://kanban.nicweb.dev",
+      text: "Kanban",
+      description: "A project tracking tool for agile teams.",
+    },
+    {
+      to: "https://checklist.nicweb.dev",
+      text: "Piloting Checklist",
+      description: "Finally, a simple checklist app for flying.",
+    },
+    {
+      to: "https://bored.nicweb.dev",
+      text: "Bored?",
+      description: "Looking for hobby or activity ideas? Try this app.",
+    },
+  ];
+
+  const RenderItems = () =>
+    links.map(({ to, text, description }) => (
+      <a href={to}>
+        <h3>{text}</h3>
+        <h4>{description}</h4>
+      </a>
+    ));
+
+  return (
+    <nav>
+      <RenderItems />
+    </nav>
+  );
 };
 
 export default Portfolio;
