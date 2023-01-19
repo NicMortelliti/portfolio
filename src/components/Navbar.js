fe8dcde4a7f28dc66c7d37ddacf62d6a4a460f04
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-19 09:42:06 -0800

    move links to own data file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FiExternalLink } from "react-icons/fi";
 
+// Data
+import { internalLinks, externalLinks } from "../data/linkData";
+
 // Styled components
 import { NavBar as Nav } from "./styles/NavBar.styled";
-import { green, red, yellow, purple } from "../components/styles/Theme";
 
 function Navbar({ setTheme }) {
   // External Links
   // Create an array of links with a "to" that points to the external url,
   // as well as the "text" to display.
   // Then map through the links, creating a link for each one.
-  const externalLinks = [
-    { to: "https://www.linkedin.com/in/nicolas-mortelliti/", text: "LinkedIn" },
-    { to: "https://github.com/NicMortelliti", text: "Github" },
-    { to: "https://dev.to/nicm", text: "Blog" },
-  ];
-
   const RenderExternalLinks = () =>
     externalLinks.map(({ to, text }, index) => (
       <a key={index} href={to}>
@@ -30,15 +26,8 @@ function Navbar({ setTheme }) {
   // Create an array of links with a "to" that points to the react-router path,
   // as well as the "text" to display as the NavLink.
   // Then map through the links, creating a navlink for each one.
-  const links = [
-    { to: "/", text: "Home", theme: green },
-    { to: "/about", text: "About", theme: red },
-    { to: "/portfolio", text: "Portfolio", theme: yellow },
-    { to: "/contact", text: "Contact Me", theme: purple },
-  ];
-
   const RenderLinks = () =>
-    links.map(({ to, text, theme }, index) => (
+    internalLinks.map(({ to, text, theme }, index) => (
       <NavLink
         key={index}
         to={to}
---
+++ src/data/linkData.js
@@ -0,0 +1,14 @@
+import { green, red, yellow, purple } from "../components/styles/Theme";
+
+export const internalLinks = [
+  { to: "/", text: "Home", theme: green },
+  { to: "/about", text: "About", theme: red },
+  { to: "/portfolio", text: "Portfolio", theme: yellow },
+  { to: "/contact", text: "Contact Me", theme: purple },
+];
+
+export const externalLinks = [
+  { to: "https://www.linkedin.com/in/nicolas-mortelliti/", text: "LinkedIn" },
+  { to: "https://github.com/NicMortelliti", text: "Github" },
+  { to: "https://dev.to/nicm", text: "Blog" },
+];
