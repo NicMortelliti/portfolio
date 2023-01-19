ba31635eec0c504011c212b1eafeed3738c1e59b
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-19 08:08:18 -0800

    fix typo

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,8 +8,8 @@ const Landing = () => {
     <Wrapper>
       <h1>Welcome!</h1>
       <p>
-        This is kind of embarrasing, you've caught in the midst of creating my
-        portfolio.
+        This is kind of embarrasing, you've caught me in the midst of creating
+        my portfolio.
       </p>
       <p>
         There isn't very much here yet, but feel free to take a look around!
