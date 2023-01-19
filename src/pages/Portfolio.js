753b182df5a4d3afe2e53abc6c680f97cc2cd986
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-18 18:36:41 -0800

    Add icons

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  HiSparkles,
+  HiTrendingUp,
+  HiClock,
+  HiBadgeCheck,
+  HiLightBulb,
+} from "react-icons/hi";
 
 const Portfolio = () => {
   const links = [
@@ -6,32 +13,38 @@ const Portfolio = () => {
       to: "https://moonshot.nicweb.dev",
       text: "Moonshot",
       description: "A fictional space flight booking site.",
+      image: <HiSparkles />,
     },
     {
       to: "https://scheduler.nicweb.dev",
       text: "Appointment Scheduler",
       description: "An application for booking appointments.",
+      image: <HiClock />,
     },
     {
       to: "https://kanban.nicweb.dev",
       text: "Kanban",
       description: "A project tracking tool for agile teams.",
+      image: <HiTrendingUp />,
     },
     {
       to: "https://checklist.nicweb.dev",
       text: "Piloting Checklist",
       description: "Finally, a simple checklist app for flying.",
+      image: <HiBadgeCheck />,
     },
     {
       to: "https://bored.nicweb.dev",
       text: "Bored?",
       description: "Looking for hobby or activity ideas? Try this app.",
+      image: <HiLightBulb />,
     },
   ];
 
   const RenderItems = () =>
-    links.map(({ to, text, description }) => (
+    links.map(({ to, image, text, description }) => (
       <a href={to}>
+        <h1>{image}</h1>
         <h3>{text}</h3>
         <h4>{description}</h4>
       </a>
