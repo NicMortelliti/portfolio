9613cd77d2ee63bad036e1da7baafbdc12656e66
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-18 18:39:17 -0800

    move data to own file

---
+++ src/data/portfolioData.js
@@ -0,0 +1,40 @@
+import {
+  HiSparkles,
+  HiTrendingUp,
+  HiClock,
+  HiBadgeCheck,
+  HiLightBulb,
+} from "react-icons/hi";
+
+export const data = [
+  {
+    to: "https://moonshot.nicweb.dev",
+    text: "Moonshot",
+    description: "A fictional space flight booking site.",
+    image: <HiSparkles />,
+  },
+  {
+    to: "https://scheduler.nicweb.dev",
+    text: "Appointment Scheduler",
+    description: "An application for booking appointments.",
+    image: <HiClock />,
+  },
+  {
+    to: "https://kanban.nicweb.dev",
+    text: "Kanban",
+    description: "A project tracking tool for agile teams.",
+    image: <HiTrendingUp />,
+  },
+  {
+    to: "https://checklist.nicweb.dev",
+    text: "Piloting Checklist",
+    description: "Finally, a simple checklist app for flying.",
+    image: <HiBadgeCheck />,
+  },
+  {
+    to: "https://bored.nicweb.dev",
+    text: "Bored?",
+    description: "Looking for hobby or activity ideas? Try this app.",
+    image: <HiLightBulb />,
+  },
+];
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,48 +1,11 @@
 import React from "react";
-import {
-  HiSparkles,
-  HiTrendingUp,
-  HiClock,
-  HiBadgeCheck,
-  HiLightBulb,
-} from "react-icons/hi";
+
+// Data
+import { data } from "../data/portfolioData";
 
 const Portfolio = () => {
-  const links = [
-    {
-      to: "https://moonshot.nicweb.dev",
-      text: "Moonshot",
-      description: "A fictional space flight booking site.",
-      image: <HiSparkles />,
-    },
-    {
-      to: "https://scheduler.nicweb.dev",
-      text: "Appointment Scheduler",
-      description: "An application for booking appointments.",
-      image: <HiClock />,
-    },
-    {
-      to: "https://kanban.nicweb.dev",
-      text: "Kanban",
-      description: "A project tracking tool for agile teams.",
-      image: <HiTrendingUp />,
-    },
-    {
-      to: "https://checklist.nicweb.dev",
-      text: "Piloting Checklist",
-      description: "Finally, a simple checklist app for flying.",
-      image: <HiBadgeCheck />,
-    },
-    {
-      to: "https://bored.nicweb.dev",
-      text: "Bored?",
-      description: "Looking for hobby or activity ideas? Try this app.",
-      image: <HiLightBulb />,
-    },
-  ];
-
   const RenderItems = () =>
-    links.map(({ to, image, text, description }) => (
+    data.map(({ to, image, text, description }) => (
       <a href={to}>
         <h1>{image}</h1>
         <h3>{text}</h3>
