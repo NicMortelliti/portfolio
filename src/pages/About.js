7a6c2425d30de92f999d0e8e456c2f3b96864d83
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-18 19:19:58 -0800

    Update landing and about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,25 @@
 import React from "react";
 
+// Styled components
+import { Wrapper } from "../components/styles/Landing.styled";
+
 const About = () => {
   return (
-    <>
+    <Wrapper>
       <h1>Hi, I'm Nic</h1>
-      <h2>
+      <p>
         I'm a newly minted software developer with a history as an avionics
         systems engineer.
-      </h2>
-      <h2>
+      </p>
+      <p>
         I'm also a father of 2, a husband, a hydroponic gardener, photographer
         and pilot.
-      </h2>
-      <h2>
+      </p>
+      <p>
         Take a look at my portfolio, if you like what you see and want to work
         together, let's get in touch.
-      </h2>
-    </>
+      </p>
+    </Wrapper>
   );
 };
 
