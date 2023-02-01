8da19eec59be3225adeb3ac369d9750a115432d7
Nic Mortelliti <npmortelliti@gmail.com>
2023-01-31 16:36:14 -0800

    adjust about me text

--- src/data/bioData.js
+++ src/data/bioData.js
@@ -1,7 +1,7 @@
 export const data = [
-  "I'm a newly minted software developer with a history as an avionics systems engineer.",
-  "I'm also a father of 2, a husband, a hydroponic gardener, photographer and pilot.",
-  "Take a look at my portfolio, if you like what you see and want to work together, let's get in touch.",
+  "I’m a web developer, systems engineer, human factors and usability expert and pilot who is easily excited by airplanes. I’m constantly exploring new technologies and applying my knowledge and skills to both professional and personal projects.",
+  "I possess a well - tuned, tenaciously analytical approach to problem solving and I’m a promoter of positivity and excellence in and out of the workplace.",
+  "Additionally, I proudly hold a reputation for being able to swiftly change priorities and focus to meet business demands.",
 ];
 
 export const email = "nic@nicweb.dev";
