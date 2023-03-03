04994bc6fc00bcd38526a1be0d44959b7fdcd881
NicMortelliti <npmortelliti@gmail.com>
2023-03-03 14:52:06 -0800

    handle onclick

--- src/components/ui/Navigation/NavLink.js
+++ src/components/ui/Navigation/NavLink.js
@@ -1,23 +1,40 @@
 import React from "react";
 import { NavLink as RouterLink } from "react-router-dom";
 
+// Styled components
 import {
   NavLink as Link,
   NavLinkExtended as LinkExtended,
 } from "../../styles/Buttons.styled";
 
+// Redux
+import { useDispatch } from "react-redux";
+import { setNavMenuIsOpen } from "../../../features/ui/uiSlice";
+
 const NavLink = ({ link, extended }) => {
+  const dispatch = useDispatch();
+
   switch (extended) {
     case true:
       return (
-        <LinkExtended as={RouterLink} to={link.to}>
+        <LinkExtended
+          as={RouterLink}
+          to={link.to}
+          onClick={() => dispatch(setNavMenuIsOpen())}>
           {link.text}
         </LinkExtended>
       );
 
     default:
       return (
-        <Link as={RouterLink} to={link.to}>
+        // The onClick action isn't totally necessary here, but in cases where
+        // a user might open a nav menu in phone portrait mode, rotate to landscape,
+        // navigate somewhere via the menu, then rotate back to portrait. If the onClick
+        // is not handled, the mobile nav menu doesn't get closed.
+        <Link
+          as={RouterLink}
+          to={link.to}
+          onClick={() => dispatch(setNavMenuIsOpen())}>
           {link.text}
         </Link>
       );
