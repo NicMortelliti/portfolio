1cbfbb5ef7750125f089a939d0b5e53bf7d7497e
Nic Mortelliti <npmortelliti@gmail.com>
2023-04-07 18:00:26 +0000

    add contact form mvp

---
+++ src/pages/Contact/ContactConfirmation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactConfirmation = () => {
+  return (
+    <div>
+      <h1>Thank you!</h1>
+      <h3>I'll be in touch with you shortly!</h3>
+    </div>
+  );
+};
+
+export default ContactConfirmation;
--- src/pages/Contact/ContactForm.js
+++ src/pages/Contact/ContactForm.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import ContactConfirmation from "./ContactConfirmation";
 
 const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
-}
+  const form = useRef();
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
-export default ContactForm
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_5a8wqtr",
+        "template_8iy1omo",
+        form.current,
+        "PLVv2FFJV86IooiJ1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            setShowConfirmation(true);
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const RenderForm = () => {
+    return (
+      <form ref={form} onSubmit={sendEmail}>
+        <label htmlFor="user_name">Name</label>
+        <input type="text" name="user_name" />
+        <label htmlFor="user_email">Email</label>
+        <input type="email" name="user_email" />
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="" cols="30" rows="10" />
+        <input type="submit" value="Send" />
+      </form>
+    );
+  };
+
+  return (
+    <div>{showConfirmation ? <ContactConfirmation /> : <RenderForm />}</div>
+  );
+};
+
+export default ContactForm;
