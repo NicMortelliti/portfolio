d3805aa8e8b21e8f1f16b8d7ab1421d834ab36fe
Nic Mortelliti <npmortelliti@gmail.com>
2023-08-23 15:57:33 -0700

    add simple toggle template

---
+++ src/components/ui/Buttons/Toggle.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Switch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+`;
+
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  transition: 0.4s;
+
+  &:before {
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: 0.4s;
+  }
+`;
+
+const Input = styled.input`
+  display: none;
+  opacity: 0;
+  position: absolute;
+
+  &:checked + ${Slider} {
+    background-color: #2196f3;
+  }
+
+  &:focus + ${Slider} {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  &:checked + ${Slider}:before {
+    transform: translateX(26px);
+  }
+`;
+
+export const Toggle = ({ opt1, opt2 }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleChange = (e) => setIsChecked(e.target.checked);
+
+  return (
+    <Switch>
+      <Input type='checkbox' checked={isChecked} onChange={handleChange} />
+      <Slider />
+    </Switch>
+  );
+};
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,11 +5,13 @@ import ContactForm from './ContactForm';
 // Styled components
 import { Wrapper } from '../../components/styles/Contact/Contact.styled';
 import { PageTitle } from '../../components/styles/Layout.styled';
+import { Toggle } from '../../components/ui/Buttons/Toggle';
 
 const Contact = () => {
   return (
     <Wrapper>
       <PageTitle>Contact Me</PageTitle>
+      <Toggle opt1='Calendly' opt2='Email' />
       <ContactForm />
       <Socials />
       <Calendly />
