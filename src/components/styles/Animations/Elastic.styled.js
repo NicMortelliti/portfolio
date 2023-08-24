9abade2df1941d0e54b59a875f032f6e46c1dc4f
Nic Mortelliti <npmortelliti@gmail.com>
2023-08-24 14:54:44 -0700

    modify stretch animation

---
+++ src/components/styles/Animations/Elastic.styled.js
@@ -0,0 +1,44 @@
+import { keyframes } from 'styled-components';
+
+const totalWidth = 263;
+const side = 0.4 * totalWidth;
+
+export const left = keyframes`
+  0% {
+    left: 0;
+    width: ${side}px;
+  }
+  25% {
+    left: 0;
+    width: 100%;
+  }
+  50% {
+    right: 0;
+    width: 100%;
+    left: auto;
+  }
+  100% {
+    right: 0;
+    width: ${side}px;
+  }
+ `;
+
+export const right = keyframes`
+    0% {
+      right: 0;
+      width: ${side}px;
+    }
+    25% {
+      right: 0;
+      width: 100%;
+    }
+    50% {
+      right: auto;
+      width: 100%;
+      left: 0;
+    }
+    100% {
+      right: 290px;
+      width: ${side}px;
+    }
+`;
--- src/components/ui/Buttons/Toggle.js
+++ src/components/ui/Buttons/Toggle.js
@@ -1,95 +1,73 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { left, right } from '../../styles/Animations/Elastic.styled';
 
 const color = ({ theme }) => theme.color;
 const accent = ({ theme }) => theme.accent;
 
 const switchWidth = 263;
+const switchHeight = 40;
 const sliderWidth = (switchWidth / 2) * 0.9;
-const sliderHeight = 26;
+const sliderHeight = switchHeight;
 const paddingX = 4;
 const paddingY = 4;
+const side = 40;
+const duration = 1;
 
-const Switch = styled.label`
-  position: relative;
-  display: flex;
+const Label = styled.label`
+  cursor: pointer;
+  font-size: 1.5em;
   width: ${switchWidth}px;
-  height: 34px;
-  cursor: pointer;
-`;
-
-const Slider = styled.span`
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  transition: 0.4s;
+  height: ${switchHeight}px;
+  display: inline-block;
+  position: relative;
+  background: #ddddff;
+  /* text-align: left; */
+  line-height: ${sliderHeight}px;
+  box-sizing: border-box;
+  /* perspective: 300px; */
 
   &:before {
-    position: absolute;
-    content: '';
-    height: ${sliderHeight}px;
     width: ${sliderWidth}px;
-    left: ${paddingX}px;
-    bottom: ${paddingY}px;
+    height: ${sliderHeight}px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: inline-block;
+    text-align: center;
+    content: '${(props) => props.label}';
     background-color: ${accent};
-    transition: 0.4s;
+    color: ${color};
+    transition: all ${duration}s ease-in-out;
+    transform-origin: 0% 50%;
   }
 `;
 
 const Input = styled.input`
-  display: none;
-  opacity: 0;
-  position: absolute;
-
-  &:focus + ${Slider} {
-    box-shadow: 0 0 1px ${accent};
+  // False --> left
+  & + ${Label} {
+    &:before {
+      animation: ${left} ${duration}s ease-in-out forwards;
+    }
   }
 
-  &:checked + ${Slider}:before {
-    transform: translateX(${switchWidth - sliderWidth - paddingX * 2}px);
-  }
-`;
-
-export const Labels = styled.span`
-  position: absolute;
-  top: 8px;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  font-size: 12px;
-  transition: all 0.4s ease-in-out;
-
-  &:before {
-    content: '${(props) => props.opt1}';
-    position: absolute;
-    left: 5px;
-    color: ${color};
-    opacity: 1;
-    transition: all 0.4s ease-in-out;
-  }
-
-  &:after {
-    content: '${(props) => props.opt2}';
-    position: absolute;
-    right: 5px;
-    color: ${color};
-    opacity: 1;
-    transition: all 0.4s ease-in-out;
+  // True --> right
+  &:checked + ${Label} {
+    &:before {
+      animation: ${right} ${duration}s ease-in-out forwards;
+    }
   }
 `;
 
 export const Toggle = ({ opt1, opt2 }) => {
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
   const handleChange = (e) => setIsChecked(e.target.checked);
 
   return (
-    <Switch>
-      <Input type='checkbox' checked={isChecked} onChange={handleChange} />
-      <Slider />
-      <Labels opt1={opt1} opt2={opt2} />
-    </Switch>
+    <>
+      <p>{`Checked: ${isChecked}`}</p>
+      <Input type='checkbox' id='input' hidden checked={isChecked} onChange={handleChange} />
+      <Label for='input' label={isChecked ? opt2 : opt1} />
+    </>
   );
 };
